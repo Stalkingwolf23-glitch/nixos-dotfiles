d5aad96c97ffbeca55e2315f943d9635e06f7eaa
Stalkingwolf <stalkingwolf23@proton.me>
2025-10-26 16:10:15 +0800

    Improvement: Changed out starship prompt for another design

--- user/shell/starship.nix
+++ user/shell/starship.nix
@@ -1,46 +1,85 @@
-{ config, pkgs, ... }:
-
 {
   programs.starship = {
-  enable = true;
-  enableZshIntegration = true;
-  settings = {
-    #Minimal left prompt
-    add_newline = true;
-    format = "$directory$character";
-    #palette = "catppuccin_mocha";
-    #move rest of prompt to the right
-    right_format = "$git_branch$gitstatus$gitstate";
-    command_timeout = 1000;
+    enable = true;
+    enableZshIntegration = true;
+    settings = {
+      add_newline = true;
+      format = "[╭╴](fg:arrow)$username$os$git_branch(at $directory)
+[╰─](fg:arrow)$character";
+      right_format = "$cmd_duration $time";
+      command_timeout = 1000;
 
-    directory = {
-      truncation_length = 2;
-    };
+      directory = {
+        truncation_length = 2;
+        style = "fg:blue";
+        read_only_style = "fg:blue";
+        before_repo_root_style = "fg:blue";
+        truncation_symbol = "…/";
+        truncate_to_repo = true;
+        read_only = "  ";
+        format = "[$path](bold $style)[$read_only]($read_only_style) ";
+      };
 
-    character = {
-      success_symbol = "[❯](bold green)";
-      error_symbol = "[❯](bold red)";
-    };
+      os = {
+        symbols = {
+          Alpine = " ";
+          Arch = " ";
+          Debian = " ";
+          EndeavourOS = " ";
+          Fedora = " ";
+          Windows = " ";
+          Mint = " ";
+          openSUSE = " ";
+          Linux = " ";
+          Manjaro = " ";
+          NixOS = " ";
+          Pop = " ";
+          SUSE = " ";
+          Macos = " ";
+          Ubuntu = " ";
+        };
+        style = "bold blue";
+        disabled = true;
+      };
 
-    git_branch = {
-      format = "[$symbol$branch(:$remote_branch)]($style)";
-    };
+      username = {
+        style_user = "bold blue";
+        style_root = "bold orange";
+        format = "[$user](fg:$style) ";
+        disabled = false;
+        show_always = true;
+      };
 
-    git_status = {
-      format = "$all_status$ahead_behind($style)";
-      style = "bold red";
-      modified = "[ 󱑍](red)";
-      staged = "[ +$count](green)";
-      renamed = " rn(red)";
-      conflicted	= " =(red)";
-      ahead	= " ⇡(yellow)";
-      behind	= " ⇣(yellow)";
-      diverged	= " ⇕(red)";
-      up_to_date	= "";
-      untracked	= " ?(red)";
-      stashed	= " \$(red)";
-      deleted	= " ✘(red)";
+      character = {
+        success_symbol = "[](fg:arrow)";
+        error_symbol = "[](fg:red)";
+      };
+
+      time = {
+        disabled = false;
+        format = "[󱑈 $time]($style)";
+        time_format = "%H:%M";
+        style = "bold fg:time";
+      };
+
+      cmd_duration = {
+        disabled = false;
+        format = "[ $duration]($style)";
+        style = "bold fg:duration";
+        min_time = 500;
+      };
+
+      git_branch = {
+        style = "bold fg:orange";
+        symbol = " ";
+        format = "via [$symbol$branch]($style) ";
+      };
+
+      git_status = {
+        style = "fg:text_color bg:orange";
+        disabled = true;
+        format = "[ $all_status$ahead_behind ]($style)";
+      };
     };
   };
-};
 }
