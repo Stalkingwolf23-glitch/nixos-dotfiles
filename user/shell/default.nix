b943377474f88cba3ad919c91133745b996a4c51
Stalkingwolf <stakingwolf23@gmail.com>
2025-11-20 18:21:20 +0800

    Cleaned out shell imports (Others to do whenever)

---
+++ user/shell/default.nix
@@ -0,0 +1,15 @@
+{
+  imports = [
+    ./btop.nix
+    ./cli.nix
+    ./fetch.nix
+    ./gallery-dl.nix
+    ./git.nix
+    ./mpd.nix
+    ./nh.nix
+    ./sh.nix
+    ./starship.nix
+    ./term.nix
+    ./yazi.nix
+  ];
+}
