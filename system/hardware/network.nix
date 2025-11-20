1bd3d0df1acc506ab045d98c46814b00ed907ddd
Stalkingwolf <stalkingwolf23@proton.me>
2025-11-20 18:45:46 +0800

    Fixing DNS problems

--- system/hardware/network.nix
+++ system/hardware/network.nix
@@ -21,14 +21,21 @@
       "a33b83"
       "-cache-size"
       "10MB"
+      "-auto-activate"
     ];
   };
 
-  systemd.services.nextdns-activate = {
-    script = ''
-      /run/current-system/sw/bin/nextdns activate
-    '';
-    after = [ "nextdns.service" ];
-    wantedBy = [ "multi-user.target" ];
+  systemd.services.nextdns = {
+    serviceConfig = {
+      ExecStartPre = "/run/current-system/sw/bin/sleep 5";
+    };
+    after = [
+      "network-online.target"
+      "NetworkManager-wait-online.service"
+    ];
+    wants = [ "network-online.target" ];
+    restartIfChanged = false;
   };
+
+  services.resolved.enable = false;
 }
