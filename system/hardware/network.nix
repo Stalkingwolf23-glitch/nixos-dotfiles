22c0fc2071a16577364acfa8625ec916c2312ad5
Stalkingwolf <stalkingwolf23@proton.me>
2026-01-20 23:22:28 +0800

    Modification: Changing from nextdns-cli to dnscrypt to handle dns

--- system/hardware/network.nix
+++ system/hardware/network.nix
@@ -7,35 +7,41 @@
       dns = "none";
     };
     wireless.userControlled.enable = true;
+
+    nameservers = [
+      "45.90.28.128"
+      "45.90.30.128"
+    ];
+  };
+
+  services.dnscrypt-proxy = {
+    enable = true;
+    settings = {
+      sources.public-resolvers = {
+        urls = [
+          "https://raw.githubusercontent.com/DNSCrypt/dnscrypt-resolvers/master/v3/public-resolvers.md"
+          "https://download.dnscrypt.info/resolvers-list/v3/public-resolvers.md"
+        ];
+        minisign_key = "RWQf6LRCGA9i53mlYecO4IzT51TGPpvWucNSCh1CBM0QTaLn73Y7GFO3"; # See https://github.com/DNSCrypt/dnscrypt-resolvers/blob/master/v3/public-resolvers.md
+        cache_file = "/var/lib/dnscrypt-proxy/public-resolvers.md";
+      };
+      ipv6_servers = true;
+      # block_ipv6 = true;
+
+      require_dnssec = true;
+      require_nolog = true;
+      require_nofilter = false;
+
+      server_names = [ "NextDNS-a33b83" ];
+    };
+  };
+
+  systemd.services.dnscrypt-proxy2.serviceConfig = {
+    StateDirectory = "dnscrypt-proxy";
   };
 
   programs.nm-applet = {
     enable = true;
     indicator = true;
   };
-
-  services.nextdns = {
-    enable = true;
-    arguments = [
-      "-config"
-      "a33b83"
-      "-cache-size"
-      "10MB"
-      "-auto-activate"
-    ];
-  };
-
-  systemd.services.nextdns = {
-    serviceConfig = {
-      ExecStartPre = "/run/current-system/sw/bin/sleep 5";
-    };
-    after = [
-      "network-online.target"
-      "NetworkManager-wait-online.service"
-    ];
-    wants = [ "network-online.target" ];
-    restartIfChanged = false;
-  };
-
-  services.resolved.enable = false;
 }
