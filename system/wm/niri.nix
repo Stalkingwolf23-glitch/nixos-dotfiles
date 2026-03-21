05b06975896fb45da6ce9cf0909ee3aab033ac67
Stalkingwolf <stalkingwolf23@proton.me>
2026-03-21 11:15:48 +0800

    Added: gnome xdg portal for screencasting - niri-blur support as well

--- system/wm/niri.nix
+++ system/wm/niri.nix
@@ -1,26 +1,28 @@
-{ pkgs, inputs, ... }:
+{
+  pkgs,
+  inputs,
+  ...
+}:
 
 {
-  imports = [ inputs.niri.nixosModules.niri ];
-  nixpkgs.overlays = [ inputs.niri.overlays.niri ];
-  niri-flake.cache.enable = true;
+  imports = [ inputs.niri.nixosModules.default ];
+  nixpkgs.overlays = [ inputs.niri.overlays.niri-nix ];
 
   programs.niri = {
     enable = true;
-    package = pkgs.niri-unstable;
-    # config = ''
-    #   ${builtins.readFile ../../assets/config/niri.kdl}
-    # '';
+    # package = pkgs.niri-unstable;
+    package = inputs.niri-blur.packages.${pkgs.stdenv.hostPlatform.system}.default;
   };
 
   xdg.portal = {
     enable = true;
-    config.niri = {
-      "org.freedesktop.impl.portal.FileChooser" = [ "gtk" ];
-    };
+    xdgOpenUsePortal = true;
     extraPortals = [
-      pkgs.xdg-desktop-portal
       pkgs.xdg-desktop-portal-gtk
+      pkgs.xdg-desktop-portal-gnome
     ];
+    config = {
+      common.default = [ "gnome" ];
+    };
   };
 }
