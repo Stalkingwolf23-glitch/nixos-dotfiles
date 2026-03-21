e024cf1e5332d4d2e2aa7b07b7f98c65ad9a38b2
Stalkingwolf <stalkingwolf23@proton.me>
2026-03-21 11:35:12 +0800

    Moved: external file declarations moved to here

--- home.nix
+++ home.nix
@@ -1,26 +1,37 @@
 { userSettings, ... }:
 
 {
-
   imports = [
     ./user/shell
     ./user/app
-    ./user/wm
+    ./user/style
 
-    ./user/hardware/bluetooth.nix
+    # ./user/hardware/bluetooth.nix
     ./user/hardware/pipewire.nix
-    ./user/stylix.nix
+  ];
+
+  nixpkgs.overlays = [
+    (import ./overlays/rgd.nix)
   ];
 
   home = {
     username = userSettings.username;
     homeDirectory = "/home/" + userSettings.username;
 
-    stateVersion = "25.05";
-    sessionPath = [
-      "$HOME/local/scripts"
-    ];
+    stateVersion = "26.05";
   };
+
+  home.file.".config/User-dirs.dirs".text = ''
+    XDG_DESKTOP_DIR = "$HOME";
+  '';
+
+  home.file.".config/niri/config.kdl".source = ./assets/config/niri.kdl;
+  home.file.".config/equibop/themes".recursive = true;
+  home.file.".config/equibop/themes".source = ./assets/config/vesktop;
+  home.file.".config/fastfetch/compact.jsonc".source = ./assets/config/compact.jsonc;
+  home.file.".config/fastfetch/fetch.jsonc".source = ./assets/config/fetch.jsonc;
+  home.file.".local/share/avatars".recursive = true;
+  home.file.".local/share/avatars".source = ./assets/avatars;
 
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
