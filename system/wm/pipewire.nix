dd5e68640a0b35e4ebd0d7316fa22f4d346ab3cf
Stalkingwolf <stalkingwolf23@proton.me>
2026-04-09 08:32:26 +0800

    Merging: pipewire source config into systems level

--- system/wm/pipewire.nix
+++ system/wm/pipewire.nix
@@ -1,4 +1,4 @@
-{ ... }:
+{ pkgs, ... }:
 
 {
   # Pipewire
@@ -8,5 +8,43 @@
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
+    extraConfig = {
+      pipewire."99-deepfilternet.conf".text = builtins.toJSON {
+        "context.properties" = {
+          "link.max-buffers" = 16;
+          "core.daemon" = true;
+          "core.name" = "pipewire-0";
+          "module.x11.bell" = false;
+          "module.access" = true;
+          "module.jackdbus-detect" = false;
+        };
+
+        "context.modules" = [
+          {
+            name = "libpipewire-module-filter-chain";
+            args = {
+              "node.description" = "DeepFilter Noise Canceling source";
+              "media.name" = "DeepFilter Noise Canceling source";
+
+              "filter.graph".nodes = [
+                {
+                  type = "ladspa";
+                  name = "DeepFilter Mono";
+                  plugin = "${pkgs.deepfilternet}/lib/ladspa/libdeep_filter_ladspa.so";
+                  label = "deep_filter_mono";
+                  control = {
+                    "Attenuation Limit (dB)" = 100;
+                  };
+                }
+              ];
+              "audio.rate" = 48000;
+              "audio.position" = "[MONO]";
+              "capture.props"."node.passive" = true;
+              "playback.props"."media.class" = "Audio/Source";
+            };
+          }
+        ];
+      };
+    };
   };
 }
