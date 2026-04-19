c5f87482ecbc3bde87d3ec8c86835a1e1de6229c
Stalkingwolf <stalkingwolf23@proton.me>
2026-04-19 10:17:54 +0800

    Added: rmpc configs

--- user/shell/mpd.nix
+++ user/shell/mpd.nix
@@ -3,6 +3,18 @@
     enable = true;
     musicDirectory = "~/Music";
     network.startWhenNeeded = true;
+    extraConfig = ''
+      audio_output {
+        type   "pipewire"
+        name   "PipeWire"
+      }
+      audio_output {
+         type   "fifo"
+         name   "my_fifo"
+         path   "/tmp/mpd.fifo"
+         format "44100:16:2"
+      }
+    '';
   };
 
   services.mpd-mpris = {
