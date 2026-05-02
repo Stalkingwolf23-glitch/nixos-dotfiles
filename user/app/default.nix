6da3d48763cb5545678e6ff400a729dd2b031b96
Stalkingwolf <stalkingwolf23@proton.me>
2026-05-02 14:14:35 +0800

    Added: missing mangohud and check skip

--- user/app/default.nix
+++ user/app/default.nix
@@ -11,7 +11,18 @@
     ./zen.nix
     ./noctalia.nix
     ./xdg.nix
+    ./mangohud.nix
     inputs.kickstart-nixvim.homeManagerModules.default
+  ];
+
+  nixpkgs.overlays = [
+    # Skipping tests while upstream sorts it out, revert once
+    # Hydra consistently builds openldap green.
+    (final: prev: {
+      openldap = prev.openldap.overrideAttrs (_: {
+        doCheck = false;
+      });
+    })
   ];
 
   home.packages = with pkgs; [
