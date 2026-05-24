f328225b8cf18acb29819c3afc99368402617457
Stalkingwolf <stalkingwolf23@proton.me>
2026-05-24 17:20:49 +0800

    Chore: rgd hash update

--- overlays/rgd.nix
+++ overlays/rgd.nix
@@ -14,10 +14,10 @@ final: prev: {
         owner = "Rolv-Apneseth";
         repo = "rgd";
         rev = "main";
-        hash = "sha256-4g0lEiGqETb4jNyb8hcJ57y0rIIKr/Ofy69+9+ikM3A=";
+        hash = "sha256-u7z9eke1LJ6X8Cshq12QRKUOEvIZkuB9tMygQqcBmsY=";
       };
 
-      cargoHash = "sha256-LD0onGVNdM0F5Ot/trXVGKjf1YHlHYlyYXzICks8nYQ=";
+      cargoHash = "sha256-J38WQMz8z7EZB0y6QrVuUvq1C5b+NKYsr9cf2C4k5YU=";
 
       buildInputs = [ sqlite ];
       nativeBuildInputs = [ pkg-config ];
