283c2307e6dfbf7866232b260b5838720f62b7dc
Stalkingwolf <stalkingwolf23@proton.me>
2026-06-07 17:04:56 +0800

    Fixed steam config

--- system/app/steam.nix
+++ system/app/steam.nix
@@ -1,8 +1,9 @@
 { pkgs, ... }:
 
 {
+  nixpkgs.config.allowUnfree = true;
+
   programs.steam = {
-    protontricks.enable = true;
     enable = true;
     package = pkgs.steam.override {
       extraProfile = ''
@@ -12,17 +13,12 @@
   };
 
   hardware.graphics = {
+    enable = true;
     enable32Bit = true;
-    extraPackages = [ pkgs.gamescope-wsi ];
-    extraPackages32 = [ pkgs.pkgsi686Linux.gamescope-wsi ];
   };
 
   programs.gamescope = {
     enable = true;
     capSysNice = false;
-    args = [
-      "--rt"
-      "--expose-wayland"
-    ];
   };
 }
