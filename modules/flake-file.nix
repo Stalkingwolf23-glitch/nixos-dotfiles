4f74408b7413cfb6c2b7cd947f1a96598b799d0e
Stalkingwolf <stalkingwolf23@proton.me>
2026-06-26 20:42:57 +0800

    Flake: Updated noctalia flake - Use cachix branch to remove building manually - Added to nixpkgs exceptions

--- modules/flake-file.nix
+++ modules/flake-file.nix
@@ -21,16 +21,22 @@
     stylix.url = "github:danth/stylix";
     zen-browser.url = "github:0xc000022070/zen-browser-flake/beta";
     aagl.url = "github:ezKEa/aagl-gtk-on-nix";
-    noctalia.url = "github:noctalia-dev/noctalia";
+    noctalia.url = "github:noctalia-dev/noctalia/cachix";
     noctalia-greeter.url = "github:noctalia-dev/noctalia-greeter";
-    niri = { url = "git+https://codeberg.org/BANanaD3V/niri-nix"; inputs.niri-unstable.follows = "niri-unstable"; };
+    niri = {
+      url = "git+https://codeberg.org/BANanaD3V/niri-nix";
+      inputs.niri-unstable.follows = "niri-unstable";
+    };
     nix-index-database.url = "github:nix-community/nix-index-database";
   };
 
   flake-file.tack.package = pkgs: inputs.tack.packages.${pkgs.stdenv.hostPlatform.system}.default;
 
   flake-file.tack.allFollow = {
-    nixpkgs = [ "nixpkgs" ];
+    nixpkgs = [
+      "nixpkgs"
+      "noctalia"
+    ];
     niri-unstable = [ "niri-unstable" ];
     flake-parts = [ "flake-parts" ];
     nixvim = [ "nixvim" ];
