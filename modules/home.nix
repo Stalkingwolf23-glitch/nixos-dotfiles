0efeea61a60344f21af5114c386e9eabd6890e0e
Stalkingwolf <stalkingwolf23@proton.me>
2026-06-27 14:44:14 +0800

    Refactor: Simplified home.file

--- modules/home.nix
+++ modules/home.nix
@@ -21,53 +21,46 @@
     stateVersion = "26.05";
   };
 
-  home.file.".config/User-dirs.dirs".text = ''
-    XDG_DESKTOP_DIR = "$HOME";
-  '';
+  home.file = {
+    ".config/niri" = {
+      source = config.lib.file.mkOutOfStoreSymlink "/home/${userSettings.username}/local/nixos/modules/assets/config/niri";
+    };
 
-  home.file.".config/niri/animations.kdl".source =
-    config.lib.file.mkOutOfStoreSymlink "/home/${userSettings.username}/local/nixos/modules/assets/config/niri/animations.kdl";
-  home.file.".config/niri/binds.kdl".source =
-    config.lib.file.mkOutOfStoreSymlink "/home/${userSettings.username}/local/nixos/modules/assets/config/niri/binds.kdl";
-  home.file.".config/niri/blur.kdl".source =
-    config.lib.file.mkOutOfStoreSymlink "/home/${userSettings.username}/local/nixos/modules/assets/config/niri/blur.kdl";
-  home.file.".config/niri/config.kdl".source =
-    config.lib.file.mkOutOfStoreSymlink "/home/${userSettings.username}/local/nixos/modules/assets/config/niri/config.kdl";
-  home.file.".config/niri/inputs.kdl".source =
-    config.lib.file.mkOutOfStoreSymlink "/home/${userSettings.username}/local/nixos/modules/assets/config/niri/inputs.kdl";
-  home.file.".config/niri/layouts.kdl".source =
-    config.lib.file.mkOutOfStoreSymlink "/home/${userSettings.username}/local/nixos/modules/assets/config/niri/layouts.kdl";
-  home.file.".config/niri/misc.kdl".source =
-    config.lib.file.mkOutOfStoreSymlink "/home/${userSettings.username}/local/nixos/modules/assets/config/niri/misc.kdl";
-  home.file.".config/niri/outputs.kdl".source =
-    config.lib.file.mkOutOfStoreSymlink "/home/${userSettings.username}/local/nixos/modules/assets/config/niri/outputs.kdl";
-  home.file.".config/niri/window-rules.kdl".source =
-    config.lib.file.mkOutOfStoreSymlink "/home/${userSettings.username}/local/nixos/modules/assets/config/niri/window-rules.kdl";
+    ".config/noctalia/palettes/CatppuccinBlue.json".source =
+      ./assets/config/noctalia/palettes/CatppuccinBlue.json;
+    ".config/noctalia/noctalia-settings.toml".source = ./assets/config/noctalia/noctalia.toml;
 
-  home.file.".config/noctalia/palettes/CatppuccinBlue.json".source =
-    ./assets/config/noctalia/palettes/CatppuccinBlue.json;
-  home.file.".config/noctalia/noctalia-settings.toml".source = ./assets/config/noctalia/noctalia.toml;
+    ".config/equibop/themes" = {
+      recursive = true;
+      source = ./assets/config/vesktop;
+    };
 
-  home.file.".config/equibop/themes".recursive = true;
-  home.file.".config/equibop/themes".source = ./assets/config/vesktop;
+    ".config/fastfetch/compact.jsonc".source = ./assets/config/compact.jsonc;
+    ".config/fastfetch/fetch.jsonc".source = ./assets/config/fetch.jsonc;
 
-  home.file.".config/fastfetch/compact.jsonc".source = ./assets/config/compact.jsonc;
-  home.file.".config/fastfetch/fetch.jsonc".source = ./assets/config/fetch.jsonc;
+    ".local/share/avatars" = {
+      recursive = true;
+      source = ./assets/avatars;
+    };
 
-  home.file.".local/share/avatars".recursive = true;
-  home.file.".local/share/avatars".source = ./assets/avatars;
+    ".config/rmpc" = {
+      recursive = true;
+      source = ./assets/config/rmpc;
+    };
 
-  home.file.".config/rmpc".recursive = true;
-  home.file.".config/rmpc".source = ./assets/config/rmpc;
+    ".config/zen/default/chrome/userChrome.css".source =
+      config.lib.file.mkOutOfStoreSymlink "/home/${userSettings.username}/local/nixos/modules/assets/zen/userChrome.css";
 
-  home.file.".config/zen/default/chrome/userChrome.css".source =
-    config.lib.file.mkOutOfStoreSymlink "/home/${userSettings.username}/local/nixos/modules/assets/zen/userChrome.css";
+    ".config/rofi" = {
+      recursive = true;
+      source = ./assets/config/rofi;
+    };
 
-  home.file.".config/rofi".recursive = true;
-  home.file.".config/rofi".source = ./assets/config/rofi;
-
-  home.file.".local/bin".recursive = true;
-  home.file.".local/bin".source = ./assets/config/scripts;
+    ".local/bin" = {
+      recursive = true;
+      source = ./assets/config/scripts;
+    };
+  };
 
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
