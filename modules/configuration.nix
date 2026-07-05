b2aebae33e4a2e32a2a517fd62795e1525d4297e
Stalkingwolf <stalkingwolf23@proton.me>
2026-07-05 15:00:45 +0800

    Refactor: move env var to user

--- modules/configuration.nix
+++ modules/configuration.nix
@@ -8,24 +8,9 @@
   imports = recursivelyImport [
     # Include the results of the hardware scan.
     ./system/hardware-configuration.nix
-
     ./system
-
     inputs.hardware.nixosModules.common-pc-ssd
   ];
-
-  # Environmental Variables
-  environment.sessionVariables = {
-    NIXOS_OZONE_WL = "1";
-    NIXOS_XDG_OPEN_USE_PORTAL = "1";
-    EDITOR = "nvim";
-    APP2UNIT_SLICES = "a=app-graphical.slice b=background-graphical.slice s=session-graphical.slice";
-    # QT
-    QT_STYLE_OVERRIDE = "kvantum";
-    QT_WAYLAND_DISABLE_WINDOWDECORATION = "1";
-    QT_AUTO_SCREEN_SCALE_FACTOR = "1";
-    QT_QPA_PLATFORM = "wayland;xcb";
-  };
 
   # Optimization
   nix.optimise.automatic = true;
---
+++ modules/user/shell/env.nix
@@ -0,0 +1,23 @@
+{
+  userSettings,
+  ...
+}:
+
+{
+  home.sessionVariables = {
+    NIXOS_OZONE_WL = "1";
+    NIXOS_XDG_OPEN_USE_PORTAL = "1";
+    EDITOR = "hx";
+    APP2UNIT_SLICES = "a=app-graphical.slice b=background-graphical.slice s=session-graphical.slice";
+    # QT
+    QT_STYLE_OVERRIDE = "kvantum";
+    QT_WAYLAND_DISABLE_WINDOWDECORATION = "1";
+    QT_AUTO_SCREEN_SCALE_FACTOR = "1";
+    QT_QPA_PLATFORM = "wayland;xcb";
+  };
+
+  xdg.userDirs = {
+    enable = true;
+    desktop = "/home/" + userSettings.username;
+  };
+}
