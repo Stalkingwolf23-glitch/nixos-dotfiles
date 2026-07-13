60a6d67629d78d4aca48c78a3b791eae4df49dd7
Stalkingwolf <stalkingwolf23@proton.me>
2026-07-13 23:08:16 +0800

    Refactor(noctalia): Move it be handled by nix

---
+++ modules/system/wm/noctalia.nix
@@ -0,0 +1,25 @@
+{
+  systemSettings,
+  inputs,
+  ...
+}:
+
+{
+  flake-file.inputs.noctalia.url = "github:noctalia-dev/noctalia/cachix";
+  nix.settings.extra-substituters = [ "https://noctalia.cachix.org" ];
+  nix.settings.extra-trusted-public-keys = [
+    "noctalia.cachix.org-1:pCOR47nnMEo5thcxNDtzWpOxNFQsBRglJzxWPp3dkU4="
+  ];
+
+  environment.systemPackages = [
+    inputs.noctalia.packages.${systemSettings.system}.default
+  ];
+
+  imports = [
+    inputs.noctalia.nixosModules.default
+  ];
+
+  programs.noctalia = {
+    enable = true;
+  };
+}
