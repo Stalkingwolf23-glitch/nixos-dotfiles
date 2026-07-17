eeae36c26b725469600a9752ac22e21d6fa0f734
Stalkingwolf <stalkingwolf23@proton.me>
2026-07-18 01:00:43 +0800

    Removed(nvim): RFemoved mans

--- modules/user/shell/nvim.nix
+++ modules/user/shell/nvim.nix
@@ -4,11 +4,12 @@
 
   flake-file.inputs = {
     kickstart-nixvim.url = "github:Stalkingwolf23-glitch/kickstart.nixvim";
-    inputs.nixvim.url = "github:nix-community/nixvim";
+    nixvim.url = "github:nix-community/nixvim";
   };
 
   programs.nixvim = {
     enable = true;
     nixpkgs.source = inputs.nixpkgs;
+    enableMan = false;
   };
 }
