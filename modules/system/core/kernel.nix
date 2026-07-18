4b588bc581b55ce305c308f06682b9554895c596
Stalkingwolf <stalkingwolf23@proton.me>
2026-07-19 01:03:31 +0800

    Added: manual TTY theming

--- modules/system/core/kernel.nix
+++ modules/system/core/kernel.nix
@@ -23,7 +23,7 @@
       };
       efi.canTouchEfiVariables = true;
       efi.efiSysMountPoint = "/boot";
-      timeout = 0;
+      timeout = 1;
     };
     plymouth = {
       enable = true;
@@ -50,6 +50,9 @@
       "split_lock_detect=off"
       "boot.shell_on_fail"
       "rd.systemd.show_status=auto"
+      "vt.default_red=30,243,166,249,137,245,148,186,88,243,166,249,137,245,148,166"
+      "vt.default_grn=30,139,227,226,180,194,226,194,91,139,227,226,180,194,226,173"
+      "vt.default_blu=46,168,161,175,250,231,213,222,112,168,161,175,250,231,213,200"
     ];
   };
 }
