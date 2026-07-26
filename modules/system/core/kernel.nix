1ea17a70ebe52bace14087a8cadc4ef2fe57b245
Stalkingwolf <stalkingwolf23@proton.me>
2026-07-26 20:50:27 +0800

    refactor(boot): drop plymouth, adjust bootloader timeout and kernel param

--- modules/system/core/kernel.nix
+++ modules/system/core/kernel.nix
@@ -23,10 +23,7 @@
       };
       efi.canTouchEfiVariables = true;
       efi.efiSysMountPoint = "/boot";
-      timeout = 1;
-    };
-    plymouth = {
-      enable = true;
+      timeout = 5;
     };
 
     initrd = {
@@ -49,7 +46,7 @@
       "systemd.show_status=auto"
       "split_lock_detect=off"
       "boot.shell_on_fail"
-      "rd.systemd.show_status=auto"
+      "systemd.show_status=auto"
       "vt.default_red=30,243,166,249,137,245,148,186,88,243,166,249,137,245,148,166"
       "vt.default_grn=30,139,227,226,180,194,226,194,91,139,227,226,180,194,226,173"
       "vt.default_blu=46,168,161,175,250,231,213,222,112,168,161,175,250,231,213,200"
