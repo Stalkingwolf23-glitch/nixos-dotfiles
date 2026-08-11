1558657874f4690e710c0528d6bee210877fd527
Stalkingwolf <stalkingwolf23@proton.me>
2026-08-11 09:27:41 +0800

    chore(hyprland): removed some default values

--- modules/desktop/hyprland/layouts.nix
+++ modules/desktop/hyprland/layouts.nix
@@ -15,9 +15,7 @@
 
             scrolling = {
               column_width = 0.5;
-              explicit_column_widths = "0.333, 0.5, 0.667";
               focus_fit_method = 0;
-              follow_focus = true;
               follow_min_visible = 0.4;
               wrap_focus = true;
               wrap_swapcol = true;
