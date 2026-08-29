c1fa1f489d20f07aa9dd6a0acf975efbf733c2d4
Stalkingwolf <stalkingwolf23@proton.me>
2026-08-29 12:51:37 +0800

    feat(naraka): move to stock kernel to avoid manual building

--- modules/hosts/naraka/hardware.nix
+++ modules/hosts/naraka/hardware.nix
@@ -1,18 +1,18 @@
-{ inputs, ... }:
+# { inputs, ... }:
 
 {
-  flake-file.inputs.nixos-hardware = {
-    url = "github:NixOS/nixos-hardware/master";
-    inputs.nixpkgs.follows = "nixpkgs";
-  };
+  # flake-file.inputs.nixos-hardware = {
+  #   url = "github:NixOS/nixos-hardware/master";
+  #   inputs.nixpkgs.follows = "nixpkgs";
+  # };
 
   flake.modules.nixos.naraka-hardware = { lib, ... }: {
-    imports = [ inputs.nixos-hardware.nixosModules.raspberry-pi-5 ];
+    # imports = [ inputs.nixos-hardware.nixosModules.raspberry-pi-5 ];
 
     nixpkgs.hostPlatform = "aarch64-linux";
     hardware = {
       enableRedistributableFirmware = true;
-      raspberry-pi.firmware.uboot.enable = true;
+      # raspberry-pi.firmware.uboot.enable = true;
     };
     # The pinned Raspberry Pi kernel does not build the ZFS module.
     boot.supportedFilesystems.zfs = lib.mkForce false;
