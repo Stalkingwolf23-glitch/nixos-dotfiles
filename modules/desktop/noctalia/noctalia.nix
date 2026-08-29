78a2bef9dd9a2bd3d0dd0644e9123c9a07e0ffbc
Stalkingwolf <stalkingwolf23@proton.me>
2026-08-29 10:29:31 +0800

    feat(noctalia): shift from flake to nixpkgs version

--- modules/desktop/noctalia/noctalia.nix
+++ modules/desktop/noctalia/noctalia.nix
@@ -1,28 +1,10 @@
 {
-  inputs,
   self,
   ...
 }:
 
 {
-  flake-file.inputs.noctalia.url = "github:noctalia-dev/noctalia/cachix";
-
-  flake.modules.nixos.noctalia = {
-    nix.settings.extra-substituters = [ "https://noctalia.cachix.org" ];
-    nix.settings.extra-trusted-public-keys = [
-      "noctalia.cachix.org-1:pCOR47nnMEo5thcxNDtzWpOxNFQsBRglJzxWPp3dkU4="
-    ];
-  };
-
-  flake.modules.nixos.compositor.imports = [
-    self.modules.nixos.noctalia
-  ];
-
   flake.modules.homeManager.noctalia = {
-    imports = [
-      inputs.noctalia.homeModules.default
-    ];
-
     programs.noctalia = {
       enable = true;
     };
