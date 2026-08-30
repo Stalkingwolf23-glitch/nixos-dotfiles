ac66307dd5a6107f1b5a91bcf78ce036e4d6b18f
Stalkingwolf <stalkingwolf23@proton.me>
2026-08-31 00:36:38 +0800

    fix(naraka): fixed options in naraka hardware

--- modules/hosts/naraka/hardware.nix
+++ modules/hosts/naraka/hardware.nix
@@ -1,12 +1,7 @@
 {
   flake.modules.nixos.naraka-hardware = { lib, ... }: {
-
     nixpkgs.hostPlatform = "aarch64-linux";
-    hardware = {
-      enableRedistributableFirmware = true;
-      raspberry-pi.firmware.uboot.enable = true;
-    };
-    # The pinned Raspberry Pi kernel does not build the ZFS module.
+    hardware.enableRedistributableFirmware = true;
     boot.supportedFilesystems.zfs = lib.mkForce false;
   };
 }
