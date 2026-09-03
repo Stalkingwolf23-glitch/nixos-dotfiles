df1a0d8efbe41a7d00116a1bf82f1780e3eb805f
Stalkingwolf <stalkingwolf23@proton.me>
2026-09-03 20:48:37 +0800

    fix(tailscale): Added tun module for tailscale to be able to autoboot

--- modules/networking/tailscale.nix
+++ modules/networking/tailscale.nix
@@ -5,7 +5,18 @@
     services.tailscale = {
       enable = true;
     };
+
+    boot.kernelModules = [ "tun" ];
+    systemd.services.tailscaled.serviceConfig.ExecStartPre = "${pkgs.kmod}/bin/modprobe tun";
   };
 
   flake.modules.nixos.networking.imports = [ self.modules.nixos.tailscale ];
+
+  flake.modules.nixos.tailscale-preservation = {
+    preservation.preserveAt."/persist".directories = [
+      "/var/lib/tailscale"
+    ];
+  };
+
+  flake.modules.nixos.preservation.imports = [ self.modules.nixos.tailscale-preservation ];
 }
