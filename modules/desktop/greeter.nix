e70d73884fa7075b2b6ffa63a116abdca529ef58
Stalkingwolf <stalkingwolf23@proton.me>
2026-09-07 21:26:21 +0800

    feat(greeter): dropped flake since nixpkgs supports noctalia-greeter now

--- modules/desktop/greeter.nix
+++ modules/desktop/greeter.nix
@@ -1,31 +1,23 @@
-{
-  inputs,
-  self,
-  ...
-}:
+{ self, ... }:
 
 {
-  flake-file.inputs.noctalia-greeter.url = "github:noctalia-dev/noctalia-greeter";
-
   flake.modules.nixos.greeter =
     { pkgs, ... }:
     {
-      imports = [ inputs.noctalia-greeter.nixosModules.default ];
-
-      programs.noctalia-greeter = {
+      services.displayManager.noctalia-greeter = {
         enable = true;
         settings = {
-          cursor = {
-            theme = "Bibata-Modern-Classic";
-            size = 24;
-            path = "${pkgs.bibata-cursors}/share/icons";
-          };
           session.default = "hyprland";
           user.default = "stalkingwolf";
           appearance = {
             hide_logo = true;
           };
           output.name = "DP-1";
+          cursorTheme = {
+            package = pkgs.bibata-cursors;
+            name = "Bibata-Modern-Classic";
+          };
+
         };
       };
 
