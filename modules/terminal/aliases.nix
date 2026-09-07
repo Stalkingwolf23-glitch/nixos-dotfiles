1d4d84d8924b0f7bb42ddc51cf62e9fd8a0e6cdb
Stalkingwolf <stalkingwolf23@proton.me>
2026-09-07 21:24:48 +0800

    feat(alias): cleaned out unused aliases and made it more general

--- modules/terminal/aliases.nix
+++ modules/terminal/aliases.nix
@@ -4,18 +4,12 @@
   flake.modules.homeManager.aliases = {
     home.shellAliases = {
       cat = "bat";
-      fetch = "fastfetch --config ~/.config/fastfetch/fetch.jsonc";
       nixos = "z ~/local/nixos";
-      oc = "z ~/.config/opencode";
       tack = "z ~/local/nixos && command tack";
       write-tack = "z ~/local/nixos && nix run .#write-tack";
-      games = "rgd list | fzf -d $'\t' --with-nth 1 | cut -d$'\t' -f2";
-      lil = "steam-run /mnt/wd_linux/Stuff/Misc/.Games/LessonsInLove/LessonsInLove0.51.0.sh";
       df = "duf";
-      update = "z ~/local/nixos && git add --intent-to-add . && nh os switch";
       ns = "nix-search-tv print | fzf --preview 'nix-search-tv preview {}' --scheme history";
       clean = "nh clean all --keep 5 --optimise";
-      export = "noctalia config export >> ~/local/nixos/modules/desktop/noctalia/export.toml";
     };
   };
 
