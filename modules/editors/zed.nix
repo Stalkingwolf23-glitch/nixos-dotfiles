2c6cdb7d15d7a9d83ef3f9ab516ad2ac393b7300
Stalkingwolf <stalkingwolf23@proton.me>
2026-09-08 23:36:48 +0800

    feat(zed): added lsp and lang server

--- modules/editors/zed.nix
+++ modules/editors/zed.nix
@@ -1,16 +1,43 @@
 {
-  flake.modules.homeManager.zed = {
+  flake.modules.homeManager.zed = { pkgs, ...}: {
     programs.zed-editor = {
       enable = true;
+      defaultEditor = true;
       mutableUserSettings = true;
       mutableUserKeymaps = false;
       mutableUserTasks = false;
-      defaultEditor = true;
       extensions = [
+        "nix"
         "catppuccin"
         "catppuccin-blur"
         "catppuccin-icons"
       ];
+      extraPackages = with pkgs; [
+        nixd
+        alejandra
+      ];
+      userSettings = {
+        languages.Nix = {
+          language_servers = [ "nixd" ];
+          formatter.external = {
+            command = "${pkgs.alejandra}/bin/alejandra";
+            arguments = [ "--quiet" "--" ];
+          };
+        };
+        lsp.nixd = {
+          binary.path = "${pkgs.nixd}/bin/nixd";
+          settings.nixd = {
+            nixpkgs.expr =
+              "import (builtins.getFlake (toString ./../..)).inputs.nixpkgs { }";
+            options.nixos.expr =
+              "(builtins.getFlake (toString ./../..)).nixosConfigurations.cocytus.options";
+            # Required for Home Manager option completion when HM is
+            # integrated into the NixOS configuration.
+            options.home-manager.expr =
+              "(builtins.getFlake (toString ./../..)).nixosConfigurations.cocytus.options.home-manager.users.type.getSubOptions []";
+          };
+        };
+      };
     };
   };
 }
