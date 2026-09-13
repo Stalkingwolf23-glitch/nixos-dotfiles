07683d2ceb81afca82cee7de39dc2bff3421bcd8
Stalkingwolf <stalkingwolf23@proton.me>
2026-09-13 09:26:30 +0800

    chore(hardware): journald configuration update

--- modules/hosts/cocytus/hardware/hardware.nix
+++ modules/hosts/cocytus/hardware/hardware.nix
@@ -1,41 +1,38 @@
 {
-  flake.modules.nixos.hardware =
-    { lib, pkgs, ... }:
-    {
-      services.journald = {
-        rateLimitBurst = 500;
-        rateLimitInterval = "30s";
-        settings.Journal = {
-          SystemMaxUse = "50M";
-          SystemMaxFiles = 5;
-          RateLimitBurst = 500;
-          RateLimitIntervalSec = "30s";
-        };
-      };
-
-      hardware.bluetooth.enable = true;
-      powerManagement.cpuFreqGovernor = "performance";
-
-      services.udev.extraRules =
-        let
-          mkRule = as: lib.concatStringsSep ", " as;
-          mkRules = rs: lib.concatStringsSep "\n" rs;
-        in
-        mkRules [
-          (mkRule [
-            ''ACTION=="add|change"''
-            ''SUBSYSTEM=="block"''
-            ''KERNEL=="sd[a-z]"''
-            ''ATTR{queue/rotational}=="1"''
-            ''RUN+="${pkgs.hdparm}/bin/hdparm -B 90 -S 41 /dev/%k"''
-          ])
-        ];
-
-      hardware.graphics = {
-        enable = true;
-        enable32Bit = true;
-      };
-
-      hardware.enableRedistributableFirmware = true;
+  flake.modules.nixos.hardware = {
+    lib,
+    pkgs,
+    ...
+  }: {
+    services.journald.settings.Journal = {
+      SystemMaxUse = "50M";
+      SystemMaxFiles = 5;
+      RateLimitBurst = 500;
+      RateLimitIntervalSec = "30s";
     };
+
+    hardware.bluetooth.enable = true;
+    powerManagement.cpuFreqGovernor = "performance";
+
+    services.udev.extraRules = let
+      mkRule = as: lib.concatStringsSep ", " as;
+      mkRules = rs: lib.concatStringsSep "\n" rs;
+    in
+      mkRules [
+        (mkRule [
+          ''ACTION=="add|change"''
+          ''SUBSYSTEM=="block"''
+          ''KERNEL=="sd[a-z]"''
+          ''ATTR{queue/rotational}=="1"''
+          ''RUN+="${pkgs.hdparm}/bin/hdparm -B 90 -S 41 /dev/%k"''
+        ])
+      ];
+
+    hardware.graphics = {
+      enable = true;
+      enable32Bit = true;
+    };
+
+    hardware.enableRedistributableFirmware = true;
+  };
 }
